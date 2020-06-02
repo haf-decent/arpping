96a69020bba98de8ce18d48aa3cd5c10fc67428d
Ryan Hafner <r.hafner20@gmail.com>
2020-06-02 19:38:43 -0400

    switch to class-ish structure, bring search methods out of object, add cache

--- index.js
+++ index.js
@@ -1,27 +1,110 @@
 'use strict';
 
 const os = require('os');
-const {exec} = require('child_process');
+const { exec } = require('child_process');
 
 const macLookup = require('./macLookup.js');
 
 var flag,
+    ipCommand,
     osType = os.type();
 
-if (osType == 'Linux' || osType == 'Windows_NT') flag = '-w';
-else if (osType == 'Darwin') flag = '-t';
-else throw new Error('Unsupported OS: ' + osType);
+switch(osType) {
+    case 'Windows_NT':
+        flag = '-w';
+        ipCommand = 'ipconfig';
+        break;
+    case 'Linux':
+        flag = '-w';
+        ipCommand = 'ifconfig';
+        break;
+    case 'Darwin':
+        flag = '-t';
+        ipCommand = 'ifconfig';
+        break;
+    default:
+        throw new Error('Unsupported OS: ' + osType);
+}
+
+function Arpping({ timeout = 5, includeEndpoints = false, useCache = true, cacheTimeout = 3600 }) {
+    if (timeout < 1 || timeout > 60) throw new Error(`Invalid timeout parameter: ${timeout}. Timeout should be between 1 and 60.`);
+    this.timeout = parseInt(timeout) || timeout.toFixed(0);
+
+    this.includeEndpoints = includeEndpoints;
+    this.myIP = null;
+
+    this.useCache = useCache;
+    this.cache = [];
+    this.cacheTimeout = cacheTimeout;
+    this.cacheUpdate = 0;
+}
 
 /**
 * Build array of full ip range (xxx.xxx.x.1-255) given example ip address
 * @param {String} ip
 */
-function getFullRange(ip) {
-    ip = ip || arpping.myIP;
+Arpping.prototype._getFullRange = function(ip) {
+    ip = ip || this.myIP;
     var ipStart = ip.substr(0, ip.lastIndexOf('.') + 1);
-    return arpping.includeEndpoints ? 
-        Array.from({length: 255}, (el, i) => ipStart + (i + 1)):
-        Array.from({length: 253}, (el, i) => ipStart + (i + 2));
+    return this.includeEndpoints ? 
+        Array.from({ length: 255 }, (_, i) => ipStart + (i + 1)):
+        Array.from({ length: 253 }, (_, i) => ipStart + (i + 2));
+}
+
+/**
+* Find ip and mac addresses of host device
+* @param {Function} callback
+*/
+Arpping.prototype.findMyInfo = function(callback) {
+    exec(ipCommand, (err, stdout, stderr) => {
+        if (err) return callback(err);
+
+        var output = null;
+        if (osType == 'Linux') {
+            if (stdout.indexOf('wlan0') == -1) return callback(new Error('No wifi connection'));
+            output = stdout.split('wlan0')[1];
+        }
+        else {
+            output = stdout.slice(stdout.indexOf('en0'));
+            output = output.slice(0, output.indexOf('active\n')) + 'active';
+            if (output.split('status: ')[1] == 'inactive') return callback(new Error('No wifi connection'));
+        }
+        var ip = output.slice(output.indexOf('inet ') + 5, output.indexOf(' netmask')).trim();
+        var mac = output.slice(output.indexOf('ether ')).split('\n')[0].split(' ')[1].trim();
+        var type = macLookup(mac);
+
+        this.myIP = ip;
+        callback(null, type ? { ip, mac }: { ip, mac, type });
+    });
+}
+
+/**
+* Discover all hosts connected to your local network or based on a reference IP address
+* @param {String} refIP
+* @param {Function} callback
+* @param {Boolean} retry
+*/
+Arpping.prototype.discover = function(refIP, callback, retry = true) {
+    if (this.useCache && this.cache.length && Date.now() - this.cacheUpdate < this.cacheTimeout * 1000) return callback(null, this.cache);
+    if (!refIP && !this.myIP) {
+        if (retry) return this.findMyInfo((err, info) => {
+            if (err) return callback(err);
+            this.discover(info.ip, callback, false);
+        });
+        return callback(new Error('Failed to find host IP address'));
+    }
+
+    var range = this._getFullRange(refIP || this.myIP);
+    this.ping(range, (err1, hosts) => {
+        if (err1) return callback(err);
+        if (!hosts.length) return callback(null, []);
+        this.arp(hosts, (err2, hosts) => {
+            if (err2) return callback(err2);
+            this.cache = hosts.slice(0);
+            this.cacheUpdate = Date.now();
+            callback(null, hosts);
+        });
+    });
 }
 
 /**
@@ -29,20 +112,16 @@ function getFullRange(ip) {
 * @param {Array} range
 * @param {Function} callback
 */
-function pingDevices(range, callback) {
+Arpping.prototype.ping = function(range, callback) {
     if (!(Array.isArray(range) && range.length)) {
-        if (!arpping.myIP) return arpping.findMyInfo(() => pingDevices(range, callback));
-        range = getFullRange();
+        if (!this.myIP) return this.findMyInfo(() => this.ping(range, callback));
+        range = this._getFullRange();
     }
-    
     var found = [],
         missing =[],
         checked = 0;
-    
-    var args = ['ping', flag, arpping.timeout];
-    range.forEach((ip) => {
-        args[3] = ip;
-        exec(args.join(' '), (err, stdout, stderr) => {
+    range.forEach(ip => {
+        exec(`ping ${flag} ${this.timeout} ${ip}`, (err, stdout, stderr) => {
             checked++;
             if (err || stdout.indexOf('100% packet loss') > -1) missing.push(ip);
             else found.push(ip);
@@ -57,25 +136,26 @@ function pingDevices(range, callback) {
 * @param {Array} range
 * @param {Function} callback
 */
-function arpDevices(range, callback) {
-    if (!Array.isArray(range)) return callback(new Error('range must be an array of IP addresses'));
-    if (!range.length) return callback(new Error('range must not be empty'));
+Arpping.prototype.arp = function(range, callback) {
+    if (typeof range == 'string') range = [ range ];
+    else if (!Array.isArray(range)) return callback(new Error('range must be an array of IP addresses'));
+    else if (!range.length) return callback(new Error('range must not be empty'));
     
     var hosts = [],
         missing = [],
         checked = 0;
-    
-    range.forEach(function(ip) {
-        exec('arp ' + ip, (err, stdout, stderr) => {
+    range.forEach(ip => {
+        exec(`arp ${ip}`, (err, stdout, stderr) => {
             checked++;
             if (err || stdout.indexOf('no entry') > -1) missing.push(ip);
             else {
-                var host = {};
-                host.ip = ip;
-                host.mac = (osType == 'Linux') ? stdout.split('\n')[1].replace(/ +/g, ' ').split(' ')[2]: stdout.split(' ')[3];
-                var known = macLookup(host.mac);
-                if (known) host.type = known;
-                if (ip == arpping.myIP) host.isYourDevice = true;
+                var mac = (osType == 'Linux') ? 
+                    stdout.split('\n')[1].replace(/ +/g, ' ').split(' ')[2]: 
+                    stdout.split(' ')[3];
+                var host = { ip, mac };
+                var type = macLookup(mac);
+                if (type) host.type = type;
+                if (ip == this.myIP) host.isHostDevice = true;
                 hosts.push(host);
             }
             
@@ -84,117 +164,67 @@ function arpDevices(range, callback) {
     });
 }
 
-var retry = false;
-var arpping = {
-    findMyInfo: function(callback) {
-        exec('ifconfig', (err, stdout, stderr) => {
-            if (err) {
-                console.log(err);
-                return callback(err);
-            }
-            var output = null;
-            if (osType == 'Linux') {
-                if (stdout.indexOf('wlan0') == -1) return callback(new Error('No wifi connection'));
-                output = stdout.split('wlan0')[1];
-            }
-            else {
-                output = stdout.slice(stdout.indexOf('en0'));
-                output = output.slice(0, output.indexOf('active\n')) + 'active';
-                if (en0.split('status: ')[1] == 'inactive') return callback(new Error('No wifi connection'));
-            }
-            var ip = output.slice(output.indexOf('inet ') + 5, output.indexOf(' netmask')).trim();
-            var mac = output.slice(output.indexOf('ether ')).split('\n')[0].split(' ')[1];
-
-            arpping.myIP = ip;
-            callback(null, { ip, mac });
-        });
-    },
-    discover: function(refIP, callback) {
-        var range = null;
-        if (refIP) {
-            range = getFullRange(refIP);
-        }
-        else if (!arpping.myIP) {
-            if (retry) {
-                retry = false;
-                return callback(new Error('Failed to find your IP address'));
-            }
-            arpping.findMyInfo((err, info) => {
-                if (err) return callback(err);
-                retry = true;
-                arpping.discover(info.ip, callback);
-            });
-            return;
-        }
-        
-        retry = false;
-        pingDevices(range, (err, range) => {
-            if (err) return callback(err);
-            arpDevices(range, (error, hosts) => {
-                if (error) return callback(error);
-                callback(null, hosts);
-            });
-        });
-    },
-    search: {
-        byIpAddress: function(ipArray, refIP, callback) {
-            if (typeof ipArray == 'string') ipArray = [ipArray];
-            else if (!Array.isArray(ipArray) || !ipArray.length) throw new Error(`Invalid ipArray: ${ipArray}. Search input should be one ip address string or an array of ip strings.`);
-            
-            arpping.discover(refIP || ipArray[0], (err, hosts) => {
-                if (err) return callback(err);
-                var check = JSON.stringify(hosts);
-                callback(
-                    null,
-                    hosts.filter(h => ipArray.indexOf(h.ip) > -1),
-                    ipArray.filter(ip => check.indexOf(ip) == -1)
-                );
-            });
-        },
-        byMacAddress: function(macArray, refIP, callback) {
-            if (typeof macArray == 'string') macArray = [macArray];
-            else if (!Array.isArray(macArray) || !macArray.length) throw new Error(`Invalid macArray: ${macArray}. Search input should be one mac address string or an array of mac address strings.`);
-            
-            arpping.discover(refIP, (err, hosts) => {
-                if (err) return callback(err);
-                var check = JSON.stringify(hosts);
-                callback(
-                    null,
-                    hosts.filter((h) => {
-                        //Mac addresses can be partial, so filtering must be done this way
-                        for (var m of macArray) if (h.mac.indexOf(m) > -1) return true;
-                        return false;
-                    }),
-                    macArray.filter(m => check.indexOf(m) == -1)
-                );
-            })
-        },
-        byMacType: function(macType, refIP, callback) {
-            arpping.discover(refIP, (err, hosts) => {
-                if (err) return callback(err);
-                callback(null, hosts.filter(h => h.type == macType));
-            });
-        }
-    },
-    ping: pingDevices,
-    arp: arpDevices,
-    myIP: null,
-    timeout: 10,
-    includeEndpoints: false
-}
-
-
 /**
-* Initialize arpping with a timeout for ping commands
-* @param {Number} t
+* Search for one or multiple IP addresses
+* @param {String/Array} ipArray
+* @param {String} refIP
+* @param {Function} callback
 */
-module.exports = function(options) {
-    if (options.timeout) {
-        if (options.timeout < 1 || options.timeout > 60) throw new Error(`Invalid timeout: ${options.timeout}. Please choose a timeout between 1 and 60s`);
-        else arpping.timeout = parseInt(options.timeout) || options.timeout.toFixed(0);
+Arpping.prototype.searchByIpAddress = function(ipArray, refIP, callback) {
+    if (typeof ipArray === 'string') ipArray = [ ipArray ];
+    else if (!Array.isArray(ipArray) || !ipArray.length) {
+        return callback(new Error(`Invalid ipArray: ${ipArray}. Search input should be one ip address string or an array of ip strings.`));
     }
     
-    if (options.includeEndpoints) arpping.includeEndpoints = true;
+    this.discover(refIP || ipArray[0], (err, hosts) => {
+        if (err) return callback(err);
+        var hostIps = hosts.map(h => h.ip);
+        callback(
+            null, 
+            hosts.filter(h => ipArray.includes(h.ip)), 
+            ipArray.filter(ip => !hostIps.includes(ip))
+        );
+    });
+}
+
+/**
+* Search for one or multiple, full or partial mac addresses
+* @param {String/Array} macArray
+* @param {String} refIP
+* @param {Function} callback
+*/
+Arpping.prototype.searchByMacAddress = function(macArray, refIP, callback) {
+    if (typeof macArray == 'string') macArray = [ macArray ];
+    else if (!Array.isArray(macArray) || !macArray.length) {
+        throw new Error(`Invalid macArray: ${macArray}. Search input should be one mac address string or an array of mac address strings.`);
+    }
     
-    return arpping;
-};
+    this.discover(refIP, (err, hosts) => {
+        if (err) return callback(err);
+        var check = JSON.stringify(hosts);
+        callback(
+            null,
+            hosts.filter(h => {
+                h.matched = [];
+                for (var m of macArray) if (h.mac.indexOf(m) > -1) h.matched.push(m);
+                return h.matched.length;
+            }),
+            macArray.filter(m => check.indexOf(m) == -1)
+        );
+    });
+}
+
+/**
+* Search for devices with the designated mac address type
+* @param {String} macType
+* @param {String} refIP
+* @param {Function} callback
+*/
+Arpping.prototype.searchByMacType = function(macType, refIP, callback) {
+    this.discover(refIP, (err, hosts) => {
+        if (err) return callback(err);
+        callback(null, hosts.filter(h => h.type == macType));
+    });
+}
+
+module.exports = Arpping;
--- test.js
+++ test.js
@@ -1,110 +1,81 @@
 'use strict';
 
-const arpping = require('./index.js')({
-    timeout: 8,
-    includeEndpoints: true
+const Arpping = require('./index.js');
+var arpping = new Arpping({
+    timeout: 4,
+    includeEndpoints: true,
+    useCache: true,
+    cacheTimeout: 30
 });
 
-function test(fn, args, callback) {
-    var start = Date.now();
-    var cb = function() {
-        var time = (Date.now() - start)/1000;
-        callback(time, ...arguments);
+const tests = {
+    findMyInfo: (err, info) => {
+        console.log('\n--------------------------------');
+        if (err) return console.log('Error during findMyInfo: ' + err);
+        console.log(info);
+        console.log(`\nFinished finding info in ${(Date.now() - start)/1000}s`);
+    },
+    discover: (err, hosts) => {
+        console.log('\n--------------------------------');
+        if (err) return console.log('Error during discover: ' + err);
+        console.log(JSON.stringify(hosts, null, 4));
+        console.log(`\nFinished discover scan in ${(Date.now() - start)/1000}s`);
+    },
+    searchByIpAddress: (err, found, missing) => {
+        console.log('\n--------------------------------');
+        if (err) return console.log('Error during searchByIpAddress: ' + err);
+        console.log(`Found ${found.length} host(s):\n${JSON.stringify(found, null, 4)}`);
+        console.log(`${missing.length} host(s) missing:\n${missing}`);
+        console.log(`\nFinished search by ip in ${(Date.now() - start)/1000}s`);
+    },
+    searchByMacAddress: (err, found, missing) => {
+        console.log('\n--------------------------------');
+        if (err) return console.log('Error during searchByMacAddress: ' + err);
+        console.log(`Found ${found.length} host(s):\n${JSON.stringify(found, null, 4)}`);
+        console.log(`${missing.length} host(s) missing:\n${missing}`);
+        console.log(`\nFinished search by mac in ${(Date.now() - start)/1000}s`);
+    },
+    searchByMacType: (err, found) => {
+        console.log('\n--------------------------------');
+        if (err) return console.log('Error during searchByMacType: ' + err);
+        console.log(`Found ${found.length} host(s):\n${JSON.stringify(found, null, 4)}`);
+        console.log(`\nFinished search by macType in ${(Date.now() - start)/1000}s`);
+    },
+    ping: (err, found, missing) => {
+        console.log('\n--------------------------------');
+        if (err) return console.log('Error during ping: ' + err);
+        console.log(`Found ${found.length} host(s):\n${found.join('\n')}`);
+        console.log(`${missing.length} host(s) missing:\n${missing}`);
+        console.log(`\nFinished ping in ${(Date.now() - start)/1000}s`);
+    },
+    arp: (err, found, missing) => {
+        console.log('\n--------------------------------');
+        if (err) return console.log('Error during arp: ' + err);
+        console.log(`Found ${found.length} host(s):\n${JSON.stringify(found, null, 4)}`);
+        console.log(`${missing.length} host(s) missing:\n${missing}`);
+        console.log(`\nFinished arp in ${(Date.now() - start)/1000}s`);
     }
-    args.push(cb);
-    fn(...args);
 }
 
-//Find devices of the same type (from the same vendor) as your device
-function example(callback) {
+var start = Date.now();
+var input = process.argv;
+
+if (input[2] == 'example') {
     console.log('Finding devices on your network with the same macType as your device...');
     arpping.findMyInfo((err, info) => {
         if (err) return console.log(err);
-        arpping.arp([info.ip], (err, found) => {
+        if (info.type) arpping.searchByMacType(info.type, null, (err, found) => {
             if (err) return console.log(err);
-            if (found[0] && found[0].type) 
-                arpping.search.byMacType(found[0].type, null, callback);
-            else console.log('No type found for your device');
+            console.log(`Found ${found.length} host(s) with your Mac Type (${host[0].type}):\n${JSON.stringify(found, null, 4)}`);
         });
-    })
+        else console.log(`No mac type found for your device`);
+    });
 }
-
-var tests = {
-    example: () => {
-        test(example, [], (time, err, found) => {
-            if (err) return console.log(err);
-            console.log(time);
-            console.log(found);
-        });
-    },
-    findMyInfo: () => {
-        test(arpping.findMyInfo, [], (time, err, info) => {
-            console.log('\n--------------------------------');
-            if (err) return console.log('Error during findMyInfo: ' + err);
-            console.log(info);
-            console.log(`\nFinished finding info in ${time}s`);
-        });
-    },
-    discover: (refIP) => {
-        test(arpping.discover, [refIP], (time, err, hosts) => {
-            console.log('\n--------------------------------');
-            if (err) return console.log('Error during discover: ' + err);
-            console.log(JSON.stringify(hosts, null, 4));
-            console.log(`\nFinished discover scan in ${time}s`);
-        });
-    },
-    searchByIpAddress: (ips, refIP) => {
-        test(arpping.search.byIpAddress, [ips, refIP], (time, err, found, missing) => {
-            console.log('\n--------------------------------');
-            if (err) return console.log('Error during searchByIpAddress: ' + err);
-            console.log(`Found ${found.length} hosts:\n${JSON.stringify(found, null, 4)}`);
-            console.log(`${missing.length} hosts missing:\n' ${missing}`);
-            console.log(`\nFinished search by ip in ${time}s`);
-        });
-    },
-    searchByMacAddress: (macs, refIP) => {
-        test(arpping.search.byMacAddress, [macs, refIP], (time, err, found, missing) => {
-            console.log('\n--------------------------------');
-            if (err) return console.log('Error during searchByMacAddress: ' + err);
-            console.log(`Found ${found.length} hosts:\n${JSON.stringify(found, null, 4)}`);
-            console.log(`${missing.length} hosts missing:\n' ${missing}`);
-            console.log(`\nFinished search by mac in ${time}s`);
-        });
-    },
-    searchByMacType: (type, refIP) => {
-        test(arpping.search.byMacType, [type, refIP], (time, err, found) => {
-            console.log('\n--------------------------------');
-            if (err) return console.log('Error during searchByMacType: ' + err);
-            console.log(`Found ${found.length} hosts:\n${JSON.stringify(found, null, 4)}`);
-            console.log(`\nFinished search by macType in ${time}s`);
-        });
-    },
-    ping: (ips) => {
-        test(arpping.ping, [ips], (time, err, found, missing) => {
-            console.log('\n--------------------------------');
-            if (err) return console.log('Error during ping: ' + err);
-            console.log(`Found ${found.length} hosts:\n${found.join('\n')}`);
-            console.log(`${missing.length} hosts missing:\n' ${missing}`);
-            console.log(`\nFinished ping in ${time}s`);
-        });
-    },
-    arp: (ips) => {
-        test(arpping.arp, [ips], (time, err, found, missing) => {
-            console.log('\n--------------------------------');
-            if (err) return console.log('Error during arp: ' + err);
-            console.log(`Found ${found.length} hosts:\n${JSON.stringify(found, null, 4)}`);
-            console.log(`${missing.length} hosts missing:\n' ${missing}`);
-            console.log(`\nFinished arp in ${time}s`);
-        });
-    }
-}
-
-var input = process.argv;
-if (!tests[input[2]]) return console.log(
+else if (!tests[input[2]]) return console.log(
     `Invalid command: ${input[2]} 
     \nValid commands:\n- ${Object.keys(tests).join('\n- ')}`
 );
-
-if (input[4]) tests[input[2]](input[3].split(','), input[4]);
-else if (input[3]) tests[input[2]](input[3].indexOf(',') > -1 ? input[3].split(','): input[3]);
-else tests[input[2]]();
+else if (input[4] || input[2].indexOf('search') > -1) arpping[input[2]](input[3].trim().split(','), input[4], tests[input[2]]);
+else if (input[3]) arpping[input[2]](input[3].trim().split(','), tests[input[2]]);
+else if (input[2] == 'discover') arpping[input[2]](null, tests[input[2]]);
+else arpping[input[2]](tests[input[2]]);
