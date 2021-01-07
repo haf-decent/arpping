08065b89db4d3e6393d9ab6047bdc7d1b9829b83
Ryan Hafner <r.hafner20@gmail.com>
2021-01-07 18:57:08 -0500

    clean up

--- test.js
+++ test.js
@@ -6,25 +6,19 @@ const arpping = new Arpping({
     cacheTimeout: 30
 });
 
+const onFound = hosts => console.log(`Found ${hosts.length} host(s):\n${JSON.stringify(hosts, null, 4)}`);
+const onFoundAndMissing = ({ hosts, missing }) => {
+    console.log(`Found ${hosts.length} host(s):\n${JSON.stringify(hosts, null, 4)}`);
+    console.log(`${missing.length} host(s) missing:\n${missing}`);
+}
+
 const tests = {
-    discover: hosts => console.log(`${hosts.length} host(s) found:\n${JSON.stringify(hosts, null, 4)}`),
-    searchByIpAddress: ({ hosts, missing }) => {
-        console.log(`Found ${hosts.length} host(s):\n${JSON.stringify(hosts, null, 4)}`);
-        console.log(`${missing.length} host(s) missing:\n${missing}`);
-    },
-    searchByMacAddress: ({ hosts, missing }) => {
-        console.log(`Found ${hosts.length} host(s):\n${JSON.stringify(hosts, null, 4)}`);
-        console.log(`${missing.length} host(s) missing:\n${missing}`);
-    },
-    searchByMacType: hosts => console.log(`Found ${hosts.length} host(s):\n${JSON.stringify(hosts, null, 4)}`),
-    ping: ({ hosts, missing }) => {
-        console.log(`Found ${hosts.length} host(s):\n${hosts.join('\n')}`);
-        console.log(`${missing.length} host(s) missing:\n${missing}`);
-    },
-    arp: ({ hosts, missing }) => {
-        console.log(`Found ${hosts.length} host(s):\n${JSON.stringify(hosts, null, 4)}`);
-        console.log(`${missing.length} host(s) missing:\n${missing}`);
-    }
+    discover: onFound,
+    searchByIpAddress: onFoundAndMissing,
+    searchByMacAddress: onFoundAndMissing,
+    searchByMacType: onFound,
+    ping: onFoundAndMissing,
+    arp: onFoundAndMissing
 }
 
 const start = Date.now();
@@ -40,12 +34,10 @@ if (input[2] == 'example') {
     const { type = null } = arpping.myDevice;
     if (!type) return console.log(`No mac type found for your device`);
     arpping.searchByMacType(type)
-        .then(hosts => {
-            console.log(`Found ${hosts.length} host(s) with your Mac Type (${type}):\n${JSON.stringify(hosts, null, 4)}`)
-        })
+        .then(onFound)
         .catch(errHandler);
 }
-else if (!tests[input[2]]) console.log(`Invalid command: ${input[2]} \nValid commands:\n- ${Object.keys(tests).join('\n- ')}`);
+else if (!tests[ input[2] ]) console.log(`Invalid command: ${input[2]} \nValid commands:\n- ${Object.keys(tests).join('\n- ')}`);
 else {
     const args = input.slice(3);
     if (args[0]) args[0] = args[0].trim().split(',');
